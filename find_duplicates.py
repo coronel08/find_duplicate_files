cd4b5626a56197f4eec0fa4bbdccee4cfd396d86
Fidelmar Coronel <36834979+coronel08@users.noreply.github.com>
2021-02-23 12:43:52 -0800

    updated to crawl directory

--- find_duplicates.py
+++ find_duplicates.py
@@ -26,6 +26,4 @@ if __name__ == "__main__":
     # Find duplicates in main branch
         # find_duplicate_files(directory)
     # Find duplicates in sub branch
-        for sub_folder in sub_folders:
-            full_directory = (f'{directory}\{sub_folder}')
-            find_duplicate_files(full_directory)
+        find_duplicate_files(folder_name)
